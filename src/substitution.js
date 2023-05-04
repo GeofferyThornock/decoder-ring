6265d657927af9adecc2a292f762adc0feebe42f
GeofferyThornock <geofferythornock@gmail.com>
2023-05-04 14:05:22 -0500

    finished up the substitution cipher; finished the substitution tests; all passing

--- src/substitution.js
+++ src/substitution.js
@@ -4,15 +4,70 @@
 // of the anonymous function on line 6
 
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
+    // you can add any code you want within this function scope
+    const arr = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ];
+    function substitution(input, alphabet, encode = true) {
+        if (!alphabet || alphabet.length !== 26) return false;
+        let cipher = alphabet.toLowerCase().split("");
+        let str = input.toLowerCase();
+        if (new Set(cipher).size !== cipher.length) return false;
 
-  function substitution(input, alphabet, encode = true) {
-    // your solution code here
-  }
+        let result = "";
 
-  return {
-    substitution,
-  };
+        if (!encode) {
+            for (let i = 0; i < input.length; i++) {
+                if (str[i] === " ") {
+                    result += str[i];
+                    continue;
+                }
+                let index = cipher.indexOf(str[i]);
+
+                result += arr[index];
+            }
+        } else {
+            for (let i = 0; i < input.length; i++) {
+                if (str[i] === " ") {
+                    result += str[i];
+                    continue;
+                }
+                let index = arr.indexOf(str[i]);
+
+                result += cipher[index];
+            }
+        }
+        return result;
+    }
+
+    return {
+        substitution,
+    };
 })();
 
 module.exports = { substitution: substitutionModule.substitution };
